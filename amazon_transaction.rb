916626d3317aa88a76aae377f79ecc5315ed4e57
c0de13 <meganmpuppy@gmail.com>
2018-07-17 18:07:04 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,26 @@
-# Code your cash register here!
+class AmazonTransaction
+# Code your cash register here
+
+
+  def initialize(total=0, it=[],dis = 20)
+    @total = total 
+    @items = it
+    @discount = dis 
+  end 
+  
+  def total
+    @total 
+  end  
+  
+  def items
+    @items
+  end  
+
+  def discount
+    @discount
+  end   
+
+
+
+
+end 
